e38ca3a6570c92c1bc2dd580cf058d292709e137
Cem <95704587+cemgx@users.noreply.github.com>
2023-05-29 15:24:42 +0300

    Add files via upload

---
+++ sicaklikk/impl/pnr/cmd.do
@@ -0,0 +1,12 @@
+-d C:\odev\sicaklikk\impl\gwsynthesis\sicaklikk.vg
+-p GW1NZ-1-QFN48-6
+-pn GW1NZ-LV1QN48C6/I5
+-cst C:\odev\sicaklikk\src\sicaklikk.cst
+-cfg C:\odev\sicaklikk\impl\pnr\device.cfg
+-bit
+-tr
+-ph
+-timing
+-cst_error
+-correct_hold 1
+-route_maxfan 10
---
+++ sicaklikk/src/nemm.v
@@ -0,0 +1,54 @@
+module TemperatureSensor(
+  input wire clk,            // Saat sinyali
+  input wire reset,          // Sıfırlama sinyali
+  input wire dht_data,       // DHT11 veri çıkışı
+
+  output wire led_red,       // Kırmızı LED çıkışı
+  output wire led_blue       // Mavi LED çıkışı
+);
+
+  reg led_red_reg;           // Kırmızı LED durumu
+  reg led_blue_reg;          // Mavi LED durumu
+
+  reg [7:0] temperature;     // Sıcaklık verisi
+
+  // DHT11 veri okuma işlemi
+  reg [39:0] dht_shift_reg;
+  reg [7:0] dht_bit_counter;
+  reg dht_valid_data;
+
+  always @(posedge clk or posedge reset) begin
+    if (reset) begin
+      led_red_reg <= 1'b0;
+      led_blue_reg <= 1'b0;
+      temperature <= 8'h00;
+      dht_shift_reg <= 40'b0;
+      dht_bit_counter <= 8'h00;
+      dht_valid_data <= 1'b0;
+    end else begin
+      // DHT11 veri okuma işlemi
+      if (!dht_valid_data) begin
+        dht_shift_reg <= {dht_shift_reg[38:0], dht_data};
+        dht_bit_counter <= dht_bit_counter + 1'b1;
+
+        if (dht_bit_counter == 8'h40) begin
+          dht_valid_data <= 1'b1;
+          temperature <= dht_shift_reg[23:16]; // Sıcaklık verisi DHT11'de 24-31 bitler arasında yer alır
+        end
+      end
+
+      // Sıcaklık değerine göre LED kontrolü
+      if (temperature > 8'h1E) begin // 30 decimal = 1E hexadecimal
+        led_red_reg <= 1'b1;
+        led_blue_reg <= 1'b0;
+      end else begin
+        led_red_reg <= 1'b0;
+        led_blue_reg <= 1'b1;
+      end
+    end
+  end
+
+  assign led_red = led_red_reg;
+  assign led_blue = led_blue_reg;
+
+endmodule
